f361a63836d922a892e405ab8cd36c04ecb6fd4a
lalotronic <epail001@gmail.com>
2025-03-12 01:56:36 -0300

    4 subida

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 
-axios.default.baseURL = import.meta.env.VITE_API_URL;
+//axios.default.baseURL = import.meta.env.VITE_API_URL;
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
