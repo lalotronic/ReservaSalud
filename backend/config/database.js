1139a2ed3fd664e4f24c02c8cd1426e47913bd00
lalotronic <epail001@gmail.com>
2025-03-12 18:38:10 -0300

    cambio en ssl en database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,6 +2,9 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: true // Cambia esto a true en producción
+}
 });
 
 module.exports = { pool };
